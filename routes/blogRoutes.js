864edb105afe375e6b615a3205ab63513c39e991
r1oga <r1oga@tuta.io>
2020-12-21 21:50:58 +0100

    feat: serve blogs from cache

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,38 +1,55 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require('mongoose')
+const requireLogin = require('../middlewares/requireLogin')
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model('Blog')
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
-    });
+    })
 
-    res.send(blog);
-  });
+    res.send(blog)
+  })
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const redis = require('redis')
+    const redisUrl = 'redis://127.0.0.1:6379'
+    const client = redis.createClient(redisUrl)
+    const util = require('util')
+    client.get = util.promisify(client.get)
 
-    res.send(blogs);
-  });
+    // Any cached data in redis related to this query?
+    const cachedBlogs = await client.get(req.user.id)
+
+    // yes, respond immediately to request and return
+    if (cachedBlogs) {
+      console.log('serving from cache')
+      return res.send(JSON.parse(cachedBlogs))
+    }
+
+    // no, query Mongo DB, cache data, return
+    const blogs = await Blog.find({ _user: req.user.id })
+    client.set(req.user.id, JSON.stringify(blogs))
+    console.log('serving from mongo')
+    res.send(blogs)
+  })
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
+    const { title, content } = req.body
 
     const blog = new Blog({
       title,
       content,
       _user: req.user.id
-    });
+    })
 
     try {
-      await blog.save();
-      res.send(blog);
+      await blog.save()
+      res.send(blog)
     } catch (err) {
-      res.send(400, err);
+      res.send(400, err)
     }
-  });
-};
+  })
+}
