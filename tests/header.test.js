eda2021bb137f54754ae37f612d6194e07550121
r1oga <r1oga@tuta.io>
2020-12-26 17:10:26 +0100

    test: Shows logout btn when logged in

--- tests/header.test.js
+++ tests/header.test.js
@@ -24,4 +24,29 @@ describe('Header', () => {
     const url = page.url()
     expect(url).toContain('oauth2')
   })
+
+  it.only('shows logout button when signed in', async () => {
+    const id = '5fe0cfeafad0c82c079c032c'
+
+    const Buffer = require('safe-buffer').Buffer
+    const sessionObject = { passport: { user: id } }
+    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+      'base64'
+    )
+
+    const Keygrip = require('keygrip')
+    const keys = require('../config/keys')
+    const keygrip = new Keygrip([keys.cookieKey])
+    const sig = keygrip.sign(`session=${sessionString}`)
+
+    await page.setCookie({ name: 'session', value: sessionString })
+    await page.setCookie({ name: 'session.sig', value: sig })
+
+    // refresh page to simulate logging in
+    await page.goto('localhost:3000')
+
+    await page.waitFor('a[href="/auth/logout"]')
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML)
+    expect(text).toEqual('Logout')
+  })
 })
